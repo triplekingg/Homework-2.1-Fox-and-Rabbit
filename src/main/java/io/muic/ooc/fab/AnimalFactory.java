888912e0a2493a4f6f35ef81a0dbcac1796e5176
Manit Gandhi <yourgitemail@example.com>
2021-05-30 00:46:45 +0700

    -Changed if-else to switch case in AnimalFactory.java

--- src/main/java/io/muic/ooc/fab/AnimalFactory.java
+++ src/main/java/io/muic/ooc/fab/AnimalFactory.java
@@ -4,14 +4,13 @@ public class AnimalFactory {
 
 
     public Animal createAnimal(AnimalType animalType, Field field, Location location){
-        if(animalType.equals(AnimalType.RABBIT)){
-            return new Rabbit(true,field,location);
-        }
-        else if(animalType.equals(AnimalType.FOX)){
-            return new Fox(true,field,location);
-        }
-        else{
-            throw new IllegalArgumentException("Unknown animaltype")
+        switch (animalType){
+            case RABBIT:
+                return new Rabbit(true,field,location);
+            case FOX:
+                return new Fox(true,field,location);
+            default:
+                throw new IllegalArgumentException("Unknown animalType");
         }
     }
 }
