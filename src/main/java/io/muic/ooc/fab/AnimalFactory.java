082b334b33a34bc42199dc69564cd6fad8ef6095
Manit Gandhi <yourgitemail@example.com>
2021-05-31 19:17:26 +0700

    -Create methods in AnimalType.java -Create Map in AnimalFactory.java

--- src/main/java/io/muic/ooc/fab/AnimalFactory.java
+++ src/main/java/io/muic/ooc/fab/AnimalFactory.java
@@ -1,9 +1,20 @@
 package io.muic.ooc.fab;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AnimalFactory {
+
+    private static Map<AnimalType, Class> animalClassMap = new HashMap<AnimalType, Class>(){{
+        AnimalType[] animalTypes = AnimalType.values();
+        for(int i = 0; i < animalTypes.length; i++){
+            put(animalTypes[i], animalTypes[i].getAnimalClass());
+        }
+    }};
 
 
     public static Animal createAnimal(AnimalType animalType, Field field, Location location){
+        Class animalClass = animalClassMap.get(animalType);
         switch (animalType){
             case RABBIT:
                 return new Rabbit(true,field,location);
--- src/main/java/io/muic/ooc/fab/AnimalType.java
+++ src/main/java/io/muic/ooc/fab/AnimalType.java
@@ -1,16 +1,22 @@
 package io.muic.ooc.fab;
 
 public enum AnimalType {
-    RABBIT(0.08),
-    FOX(0.02);
+    RABBIT(0.08, Rabbit.class),
+    FOX(0.02,Fox.class);
 
     private double breedingProbability;
+    private Class animalClass;
 
-    AnimalType(double breedingProbability) {
+    AnimalType(double breedingProbability, Class animalClass) {
         this.breedingProbability = breedingProbability;
+        this.animalClass = animalClass;
     }
 
     public double getBreedingProbability() {
         return breedingProbability;
+    }
+
+    public Class getAnimalClass(){
+        return animalClass;
     }
 }
